81795785136dca55f63df642cf18615824b2dda5
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-09-04 11:08:38 +0300

    Добавил навигацию между страницами для header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,21 @@
+<script setup>
+const pagesList = [
+  { name: 'home', label: 'главная' },
+  { name: 'convert', label: 'конвертация' },
+];
+
+</script>
+
 <template>
   <header>
-    header
+    <nav>
+      <ul>
+        <li v-for="page in pagesList" :key="page.name">
+          <routerLink :to="{ name: page.name }">
+            {{ page.label }}
+          </routerLink>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
